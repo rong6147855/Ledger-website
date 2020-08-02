57cb392e0c9dc479622d0dcc42d13d7fc22f391a
yangrong <rong6147855@foxmail.com>
2020-08-02 20:52:35 +0800

    deploy

---
+++ precache-manifest.47dcd0e41cf111132f09413a7f2753e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9513bba6212f9728053a",
+    "url": "/Ledger-website/css/app.b8456196.css"
+  },
+  {
+    "revision": "b3b8f64b6919584b2e3c1cd5a1af7613",
+    "url": "/Ledger-website/index.html"
+  },
+  {
+    "revision": "9513bba6212f9728053a",
+    "url": "/Ledger-website/js/app.d04785c3.js"
+  },
+  {
+    "revision": "a3931921f41cd507d7ed",
+    "url": "/Ledger-website/js/chunk-vendors.1eba432a.js"
+  },
+  {
+    "revision": "216ae5fed3184cd1ded3bf6373919285",
+    "url": "/Ledger-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Ledger-website/robots.txt"
+  }
+]);
